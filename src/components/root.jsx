ab713c2a4b079bb52c9f343df3a8f1c68a699bef
Maëlys McArdle <airseal@hotmail.com>
2018-02-04 14:23:02 -0500

    Added story component.

--- src/components/root.jsx
+++ src/components/root.jsx
@@ -1,22 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import NavigationBar from './navbar.jsx';
-import StoryText from './text.jsx';
-import StoryOptions from './options.jsx';
+import Story from './story.jsx';
 
 const storyTitle = 'Adventure';
 const storyText = ['Blahhhh', 'bloo'];
 const storyOptions = ['one option', 'other option'];
 
 ReactDOM.render(
-  <div>
-    <NavigationBar title={storyTitle} />
-
-    <div className="container">
-      <StoryText paragraphs={storyText} />
-      <StoryOptions options={storyOptions} />
-    </div>
-
-  </div>,
+  <Story 
+    storyTitle={storyTitle} 
+    storyText={storyText} 
+    storyOptions={storyOptions} />,
   document.getElementById('root')
 );
