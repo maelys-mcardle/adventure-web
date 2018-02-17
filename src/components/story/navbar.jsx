34c7087287c56d129cf77c5e87abf21706442bad
Maëlys McArdle <airseal@hotmail.com>
2018-02-17 11:08:59 -0500

    Added confirmation to menu.

--- src/components/story/navbar.jsx
+++ src/components/story/navbar.jsx
@@ -1,6 +1,51 @@
 import React from 'react';
 
 export default class NavigationBar extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      showConfirmStartOver: false
+    };
+  }
+
+  setConfirmStartOver(value) {
+    this.setState({
+      showConfirmStartOver: value,
+    })
+  }
+
+  renderStartOverButton() {
+    if (this.state.showConfirmStartOver) {
+      return (
+        <button 
+          className="btn btn-outline-light" 
+          type="button"
+          onClick={() => this.setConfirmStartOver(true)}>
+          Start Over
+        </button>
+      );
+    } else {
+      return (
+        <span>
+          <strong>Are you sure?</strong>
+          <button 
+            className="btn btn-outline-danger left-spacer" 
+            type="button"
+            onClick={this.props.restart}>
+            Yes
+          </button>
+          <button 
+            className="btn btn-outline-light left-spacer" 
+            type="button"
+            onClick={() => this.setConfirmStartOver(false)}>
+            No
+          </button>
+        </span>
+      );
+    }
+  }
+
   render() {
     return (
       <nav className="navbar navbar-dark bg-primary justify-content-between">
@@ -8,12 +53,7 @@ export default class NavigationBar extends React.Component {
           "{this.props.story.title}" by {this.props.story.author}
         </span>
         <form className="form-inline">
-          <button 
-            className="btn btn-outline-light" 
-            type="button"
-            onClick={this.props.restart}>
-            Start Over
-          </button>
+          {this.renderStartOverButton()}
           <button 
             className="btn btn-outline-light left-spacer" 
             type="button"
